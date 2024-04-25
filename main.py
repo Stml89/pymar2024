538b32b5e7fa613711890f38daf4a70753398309
buzinov.yu <buzinov.yura@gmail.com>
2024-04-25 12:58:35 +0400

    The changes were made by: Yury Buzinau Changes made: - Added Docstrings in main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+"""Main module.
+
+This module contains the main function for executing the program.
+"""
+
+
 def main():
+    """Main function.
+    This function prints the greeting "Hello world!" to the console.
+    """
     print("Hello world!")
 
 
